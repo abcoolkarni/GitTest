0614eda97ccf73e4b8c9c110e5df85f1e279e6f9
abcoolkarni <kulkarnikumarabhishek@gmail.com>
2023-02-26 21:07:48 +0530

    latest

--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
-	fmt.Println(" World")
-	fmt.Println("Karthik")
-	fmt.Println("Vishwa")
-	fmt.Println("ABHI")
+	var wg sync.WaitGroup
+
+	m := new(sync.Mutex)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+
+		go func(i int) {
+			m.Lock()
+			fmt.Println(i, "start")
+
+			// do some work
+			fmt.Println(i, "working")
+
+			m.Unlock()
+			fmt.Println(i, "done")
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
 }
